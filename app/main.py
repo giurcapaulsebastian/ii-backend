c7b83bfacd33a17f0353aabe5e78f73cff5d40e6
dan-lucian <nitadanlucian@outlook.com>
2023-05-13 17:26:15 +0300

    fix: skip cors verification

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def get_application():
     app = fastapi.FastAPI(title=settings.PROJECT_NAME)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
